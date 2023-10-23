b2d67dc5131b396f9c0116a14e569110d678f966
NoeliaCI <>
2023-10-23 14:31:59 +0000

    Complete new game function

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
 # This function creates a new game when is called
 
 def new_game():
-    pass
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+
+    for key in questions:
+        print("--------------------------------------------------------------------------------------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Enter your answer (A, B, C or D): ")
+        guess = guess.upper()
+        guesses.append(guess)
+        check_answer(questions.get(key), guess)
+        question_num += 1
 
 
 # This function checks the answers provided by the user
@@ -34,12 +47,12 @@ questions = {
 options = [["A. Queen", "B. Led Zeppelin", "C. Kiss", "D. The Smashing Pumpkins"],
     ["A. 1992", "B. 1996", "C. 1995", "D. 1993"],
     ["A. Nine Inch Nails", "B. Tool", "C. Rammstein", "D. White Zombie"],
-    ["A. 1978","B. 1975", "C. 1979", "D. 1977"]
-	["A. Hurricane","B. Blowin' In The Wind", "C. Make You Feel My Love", "D. Tangled Up In Blue"]
-    ["A. Led Zeppelin","B. Genesis", "C. Cream", "D. The Who"]
-    ["A. The Pussycat Dolls","B. The Black Eyed Peas", "C. No Doubt", "D. Outkast"]
-    ["A. Björk","B. PJ Harvey", "C. Tina Turner", "D. Kate Bush"]
-    ["A. USA","B. England", "C. Australia", "D. Denmark"]
+    ["A. 1978","B. 1975", "C. 1979", "D. 1977"],
+	["A. Hurricane","B. Blowin' In The Wind", "C. Make You Feel My Love", "D. Tangled Up In Blue"],
+    ["A. Led Zeppelin","B. Genesis", "C. Cream", "D. The Who"],
+    ["A. The Pussycat Dolls","B. The Black Eyed Peas", "C. No Doubt", "D. Outkast"],
+    ["A. Björk","B. PJ Harvey", "C. Tina Turner", "D. Kate Bush"],
+    ["A. USA","B. England", "C. Australia", "D. Denmark"],
     ["A. Hozier","B. The Edge", "C. Gene Simmons", "D. Bono"]]
 
-
+new_game()
