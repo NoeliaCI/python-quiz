4762ad37942e314b63dd395a009757127ab980ae
NoeliaCI <>
2023-10-23 13:47:14 +0000

    Add main functions for running the game

--- run.py
+++ run.py
@@ -1,3 +1,45 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# This function creates a new game when is called
+
+def new_game():
+    pass
+
+
+# This function checks the answers provided by the user
+
+def check_answer():
+    pass
+# This function displays the score 
+
+def display_score():
+    pass
+# This function starts a new game when the player answers yes
+def play_again():
+    pass
+
+
+questions = {
+ "Roger Taylor is the drummer in which band?: ": "A",
+ "In which year did the Spice Girls release Wannabe?: ": "B",
+ "Which rock band was founded by Trent Reznor in 1988?: ": "A",
+ "In what year did Elvis Presley die?: ": "D",
+ "Which Bob Dylan song did Adele include on her first album?: ": "C",
+ "What was the name of the band formed by Jack Bruce, Eric Clapton, and Ginger Baker?: ": "C",
+ "Singer Fergie was previously a member of which hip hop group?: ": "B",
+ "Which singer had a charts resurgence in 2022 after one of her songs was heard in Stranger Things?: ": "D",
+ "Rock band AC/DC originates from which country?: ": "C",
+ "Rockstar David Howell Evans is better known by what name?: ": "B",
+
+}
+
+options = [["A. Queen", "B. Led Zeppelin", "C. Kiss", "D. The Smashing Pumpkins"],
+          ["A. 1992", "B. 1996", "C. 1995", "D. 1993"],
+          ["A. Nine Inch Nails", "B. Tool", "C. Rammstein", "D. White Zombie"],
+          ["A. 1978","B. 1975", "C. 1979", "D. 1977"]
+	      ["A. Hurricane","B. Blowin' In The Wind", "C. Make You Feel My Love", "D. Tangled Up In Blue"]
+          ["A. Led Zeppelin","B. Genesis", "C. Cream", "D. The Who"]
+          ["A. The Pussycat Dolls","B. The Black Eyed Peas", "C. No Doubt", "D. Outkast"]
+          ["A. Björk","B. PJ Harvey", "C. Tina Turner", "D. Kate Bush"]
+          ["A. USA","B. England", "C. Australia", "D. Denmark"]
+          ["A. Hozier","B. The Edge", "C. Gene Simmons", "D. Bono"]]
+
+
